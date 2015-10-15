7670083d15d30294cb88635234b79e6f54ac6fce
Kirill Bondar <Drix87@gmail.com>
2015-10-15 16:50:56 +0300

    Update

--- js/components/main-nav__hamburger.js
+++ js/components/main-nav__hamburger.js
@@ -1,7 +1,5 @@
-$( document ).ready(function() {
-	var toggler = document.getElementById('main-nav__hamburger');
-	toggler.onclick = function(e){
-	  e.preventDefault();
-	  toggler.classList.toggle('main-nav__hamburger--close');
-	}
-});
+var toggler = document.getElementById('main-nav__hamburger');
+toggler.onclick = function(e){
+  e.preventDefault();
+  toggler.classList.toggle('main-nav__hamburger--close');
+}
